7c881db65a004c8b4e93d00889331c38709824a1
Hemil Patel <patelhemil585@gmail.com>
2026-03-17 23:25:47 +0530

    enable automatic GC and store optimisation

--- configuration.nix
+++ configuration.nix
@@ -187,6 +187,16 @@
     };
   };
 
+  nix = {
+    settings.auto-optimise-store = true;
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
