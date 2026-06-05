5b92b03773784193043169e6d38ab3c17c42b09e
Hemil Patel <patelhemil585@gmail.com>
2026-06-05 20:59:59 +0530

    Upgrade to 26.05 Yarara

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My lil flake!";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
